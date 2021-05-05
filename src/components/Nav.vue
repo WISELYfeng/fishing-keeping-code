0bd6249bbdd2d977c27a4b568537d759076c39c2
wsielyfeng <learnroad@163.com>
2021-05-05 22:55:41 +0800

    引入svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <router-link to="/money">首页|</router-link>
+    <router-link to="/money">
+    <svg>
+      <use xlink:href="#label" />
+    </svg>
+    首页|
+    </router-link>
     <router-link to="/labels">标签|</router-link>
     <router-link to="/statistics">统计</router-link>
   </div>
 </template>
 
 <script lang="ts">
+import x from '@/assets/icons/label.svg'
+console.log(x)
 export default {};
 </script>
 
