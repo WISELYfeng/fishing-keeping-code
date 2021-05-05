7ccd2cd1be914d3590dfbcb51c6feff7e227ec56
wsielyfeng <learnroad@163.com>
2021-05-05 16:28:10 +0800

    增加404路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
+
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import Notfound from '@/views/Notfound.vue'
 
 Vue.use(VueRouter)
 
@@ -22,20 +24,11 @@ const routes: Array<RouteConfig> = [
   {
     path:'/statistics',
     component:Statistics
+  },
+  {
+    path:'*',
+    component:Notfound
   }
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Notfound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>要访问的内容不存在，请检查网址是否正确</div>
+        <router-link to="/">返回首页</router-link>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
