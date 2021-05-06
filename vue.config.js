f9c44458fe642882eb19417cc3e06fa9e24b3158
wsielyfeng <learnroad@163.com>
2021-05-06 11:51:09 +0800

    解决svg颜色不能修改的问题

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,10 @@ module.exports = {
       .test(/\.svg$/)
       .include.add(dir).end()  // 只操作icons目录
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
+      .use('svgo-loader').loader('svgo-loader') // 从这行开始的两行代码是为了解决有些图标不能通过color修改颜色的解决办法
+      .tap(options => ({...options,plugins: [{removeAttrs: {attrs:'fill'}}]})).end()
+
+      // loader插件的配置
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'),[{plainSprite:true}]
     )
     config.module.rule('svg').exclude.add(dir)  // 其他svg-loader排除icons目录
