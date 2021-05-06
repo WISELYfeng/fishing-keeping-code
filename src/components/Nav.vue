5752b9b8c6fa1e3a411df0ce0974ec787ef2cad9
wsielyfeng <learnroad@163.com>
2021-05-06 11:04:35 +0800

    封装Icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
     <router-link to="/money">
-    <svg>
-      <use xlink:href="#label" />
-    </svg>
-    首页|
+      <Icon name="money"></Icon>
+      首页
     </router-link>
-    <router-link to="/labels">标签|</router-link>
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/labels">
+      <Icon name="label"></Icon>
+      标签
+      </router-link>
+    <router-link to="/statistics">
+      <Icon name="charts"></Icon>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
-import x from '@/assets/icons/label.svg'
-console.log(x)
 export default {};
 </script>
 
