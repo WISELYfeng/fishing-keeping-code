9caba5bcc9f36248e779e0e219ebe22cc104a90a
wsielyfeng <learnroad@163.com>
2021-05-10 21:32:36 +0800

    改造Money的四个组件，整合数据处理

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,36 +1,54 @@
 <template>
   <div>
     <Layout class-prefix="layout">
-    <NumberPad></NumberPad>
-     <Types kind="-"></Types>
-     <Notes></Notes>
-     <Tags :dataSource.sync="tags"></Tags>
+      <NumberPad :value.sync="record.amount"></NumberPad>
+      <Types :value.sync="record.type"></Types>
+      <Notes @update:value="onUpdateNotes"></Notes>
+      <Tags :dataSource.sync="tags" @update:value="onUpdateTags"></Tags>
     </Layout>
   </div>
 </template>
 
 <script lang="ts">
-import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
-import Notes from '@/components/Money/Notes.vue';
-import Tags from '@/components/Money/Tags.vue';
-export default {
-  components: { NumberPad, Types, Notes, Tags },
-  data(){
-    return{
-      tags:['衣','食','住','行']
-    }
+import Vue from 'vue';
+import NumberPad from "@/components/Money/NumberPad.vue";
+import Types from "@/components/Money/Types.vue";
+import Notes from "@/components/Money/Notes.vue";
+import Tags from "@/components/Money/Tags.vue";
+import { Component } from 'vue-property-decorator';
+
+type Record = {
+  tags:string[]
+  notes:string
+  type:string
+  amount:number
+}
+@Component({
+  components:{Tags, Types,Notes,NumberPad}
+})
+export default class Money extends Vue {
+  tags = ['衣','食','住','行','旅游'];
+  record: Record = {
+    tags:[],
+    notes:'',
+    type:'-',
+    amount:0
+  };
+  onUpdateTags(value:string[]){
+    this.record.tags = value;
   }
-};
+  onUpdateNotes(value:string){
+    this.record.notes = value;
+  }
+}
 </script>
 
 <style lang="scss">
-  .layout-content{
-    display: flex;
-    flex-direction: column-reverse;
-  }
+.layout-content {
+  display: flex;
+  flex-direction: column-reverse;
+}
 </style>
 
 <style lang="scss" scoped>
-  
 </style>
