2428a0775d3a5cbc0f1258b4c76a7dc58c6670f0
wsielyfeng <learnroad@163.com>
2021-05-11 20:46:28 +0800

    增加label点击跳转路由

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <Layout>
-      <ol class="tags">
-        <li v-for="tag in tags" :key="tag.id">
+      <div class="tags">
+        <router-link class="tag" v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${id}`">
             <span>{{tag.name}}</span>
             <Icon name="right"></Icon>
-        </li>
-      </ol>
+        </router-link>
+      </div>
       <div class="create-wrapper">
           <button class="createTag" @click="createTag">
               新建标签
@@ -45,7 +45,7 @@ export default class Labels extends Vue {
     background: white;
     font-size: 16px;
     padding-left: 16px;
-    > li {
+    > .tag {
       min-height: 44px;
       display: flex;
       align-items: center;
