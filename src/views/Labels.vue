75d2f39e2a26ee31f95dabd013ef00a191bbb637
wsielyfeng <learnroad@163.com>
2021-05-11 21:17:17 +0800

    替换label的button

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -8,9 +8,9 @@
         </router-link>
       </div>
       <div class="create-wrapper">
-          <button class="createTag" @click="createTag">
+          <Button class="createTag" @click="createTag">
               新建标签
-          </button>
+          </Button>
       </div>
     </Layout>
   </div>
@@ -20,9 +20,12 @@
 import Vue from "vue";
 import tagListModel from "../models/tagListModel";
 import { Component } from "vue-property-decorator";
+import Button from '../components/Button.vue';
 
 tagListModel.fetch();
-@Component
+@Component({
+  components:{Button}
+})
 export default class Labels extends Vue {
   tags = tagListModel.data;
 
