fae0c70dd5d5a98446d52f682220b125b79a6604
wsielyfeng <learnroad@163.com>
2021-05-16 13:22:50 +0800

    创建二维码

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <router-view />
+    <Qrcode v-if="qrcodeVisible"></Qrcode>
   </div>
 </template>
 
@@ -9,12 +10,30 @@
 @import "~@/assets/style/helper.scss";
 
 body {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #333;
-    font-family: $font-hei;
-    font-size: 16px;
-    line-height: 1.5;
-    background: #f5f5f5;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #333;
+  font-family: $font-hei;
+  font-size: 16px;
+  line-height: 1.5;
+  background: #f5f5f5;
 }
 </style>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from 'vue-property-decorator';
+import Qrcode from '@/components/Qrcode.vue';
+
+@Component({
+  components:{Qrcode}
+})
+export default class App extends Vue {
+  qrcodeVisible:boolean = false;
+  created() {
+    if (document.documentElement.clientWidth > 500) {
+      this.qrcodeVisible = true;
+    }
+  }
+}
+</script>
---
+++ src/components/Qrcode.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="qrcode">
+    <img src="../assets/qrcode.png" alt="qrcode" />
+    <div class="slogan">扫描二维码👆<br />获得更佳使用体验</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.qrcode {
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+.slogan {
+  color: white;
+  font-size: larger;
+  font-weight: bolder;
+  background: #ffa3a6;
+}
+</style>
